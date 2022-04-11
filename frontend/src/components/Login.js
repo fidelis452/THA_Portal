1baf3bfdcb3f92d85f36df269068213630628ec5
fidelis <fideliswaweru19@gmail.com>
2022-04-11 16:36:53 +0300

    Backend bp

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,35 +1,107 @@
+import React, { Component } from "react";
+import {Routes,Route } from "react-router-dom";
 import {Avatar, Grid, Paper, Typography, TextField, Link, Button} from "@material-ui/core";
 import Checkbox from '@material-ui/core/Checkbox';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 
-function Login(){
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+      email: "",
+      password: "",
+      error: "",
+      redirectToHome: false,
+      loading: false,
+    };
+  }
+  //with the syntax as an array we dont an onclick func for each input
+  //takes in a parameter and an event
+  //set the value to have it as data to send to backend
+  handleChange = (email) => (event) => {
+    this.setState({ error: "" });
+    this.setState({ [email]: event.target.value });
+  };
+  clickSubmit = (event) => {
+    this.setState({ loading: true });
+    event.preventDefault();
+    const { email, password } = this.state;
+    const user = {
+      email,
+      password,
+    };
+    //get the route from backend
+    fetch("http://localhost:5000/login", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((response) => {
+        return response.json(user);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .then((data) => {
+        if (data.message) {
+          this.setState({ error: data.message, loading: false });
+        } else {
+          //authenticate the user and redirect  to home
+          this.authenticate(data, () => {
+            this.setState({ redirectToHome: true });
+          });
+        }
+      });
+  };
+  render() {
+    const { username, email,password, 
+        // error, 
+        redirectToHome, 
+        // loading
+     } = this.state;
+    if (redirectToHome) {
+      return <div>
+        <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+      </div>;
+    }
     const stylePaper={padding: 20, margin: "40px auto", width: "350px"}
     const avatarStyle={background: "blue"}
     const styleForm={padding:10,}
     const btnStyle={padding: 10}
     const styleTypo={padding:20}
-    return(
+    return (
         <Grid>
-            <Paper elevation={10} style={stylePaper}>
-                <Grid align="center" justifyContent="flex">
-                    <Avatar style={avatarStyle}>P</Avatar>
-                    <h2>SIGN IN</h2>
-                </Grid>
-                <TextField id="outlined-basic" label="Username" variant="outlined" style={styleForm} fullWidth required/>
-                <TextField id="outlined-basic" label="Email" variant="outlined" style={styleForm} fullWidth required/>
-                <TextField id="outlined-basic" label="Password" type="password" variant="outlined" style={styleForm} fullWidth required/>
-                <FormControlLabel
-        control={
-          <Checkbox name="checkedB" color="primary" />
-        }
-        label="Remember Me"
-      />
-      <Button variant="contained" color="primary" style={btnStyle} fullWidth>SIGN IN</Button>
-      <Typography align="center" style={styleTypo}>
-          <Link href="#">Forgot Password</Link>
-        </Typography>
-        </Paper>
-    </Grid>
-    )
+        <Paper elevation={10} style={stylePaper}>
+            <Grid align="center" justifyContent="flex">
+                <Avatar style={avatarStyle}>P</Avatar>
+                <h2>SIGN IN</h2>
+            </Grid>
+            <TextField id="outlined-basic" label="Username" variant="outlined"  
+            onChange={this.handleChange("username")} value={username}style={styleForm} fullWidth required/>
+            <TextField id="outlined-basic" label="Email" variant="outlined" 
+            onChange={this.handleChange("email")} value={email} style={styleForm} fullWidth required/>
+            <TextField id="outlined-basic" label="Password" type="password" 
+            onChange={this.handleChange("password")} value={password} variant="outlined" style={styleForm} fullWidth required/>
+            <FormControlLabel
+    control={
+      <Checkbox name="checkedB" color="primary" />
+    }
+    label="Remember Me"
+  />
+  <Button variant="contained" color="primary" style={btnStyle} type="button"
+            onClick={this.clickSubmit} fullWidth>SIGN IN</Button>
+  <Typography align="center" style={styleTypo}>
+      <Link href="#">Forgot Password</Link>
+    </Typography>
+    </Paper>
+</Grid>
+    );
+  }
 }
-export default Login
+export default Login;
